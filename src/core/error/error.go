a10c0d6df7146cfa4af805205c285e68fd7afdde
schwarzwald0906 <schwarzwald0906@gmail.com>
2023-01-04 11:56:57 +0900

    traceを修正

--- src/core/error/error.go
+++ src/core/error/error.go
@@ -33,15 +33,12 @@ func BadRequestWrapf(format string, args ...interface{}) error {
 
 	if len(args) == 0 {
 		err.msg = format
-		//提示いただいた例だと、errors.New()をさらにErrorf（）してましたが、
-		//Errorf（)を挟む理由がよくわからなかったので、直でやりました。
-		err.trace = errors.New(format)
+		err.trace = errors.Errorf("%+v", errors.New(format))
 		return &BadRequestErr{err}
 	}
 
 	err.msg = fmt.Sprintf(format, args...)
-	//ここも、Errorf（)にErrorf（)を挟む理由がよくわからなかったので、直でやりました。
-	err.trace = errors.Errorf(format, args...)
+	err.trace = errors.Errorf("%+v", errors.Errorf(format, args...))
 	return &BadRequestErr{err}
 }
 
@@ -52,12 +49,12 @@ func NotFoundWrapf(format string, args ...interface{}) error {
 
 	if len(args) == 0 {
 		err.msg = format
-		err.trace = errors.New(format)
+		err.trace = errors.Errorf("%+v", errors.New(format))
 		return &NotFoundErr{err}
 	}
 
 	err.msg = fmt.Sprintf(format, args...)
-	err.trace = errors.Errorf(format, args...)
+	err.trace = errors.Errorf("%+v", errors.Errorf(format, args...))
 	return &NotFoundErr{err}
 }
 
@@ -68,11 +65,11 @@ func InternalServerWrapf(format string, args ...interface{}) error {
 
 	if len(args) == 0 {
 		err.msg = format
-		err.trace = errors.New(format)
+		err.trace = errors.Errorf("%+v", errors.New(format))
 		return &InternalServerErr{err}
 	}
 
 	err.msg = fmt.Sprintf(format, args...)
-	err.trace = errors.Errorf(format, args...)
+	err.trace = errors.Errorf("%+v", errors.Errorf(format, args...))
 	return &InternalServerErr{err}
 }
