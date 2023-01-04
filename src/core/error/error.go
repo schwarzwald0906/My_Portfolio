3be02c1e685beb269d76294dcc4cf45e103a89af
schwarzwald0906 <schwarzwald0906@gmail.com>
2023-01-04 10:50:52 +0900

    Errorf（）を挟まない形に修正。

--- src/core/error/error.go
+++ src/core/error/error.go
@@ -9,35 +9,9 @@ import (
 
 type appErr struct {
 	error
-	msg    string
-	trace  error
-	status int
-}
-
-type appError interface {
-	TraceString() string
-	Error() string
-	Status() int
-	AppendTrace(err error)
-}
-
-func (e *appErr) TraceString() string {
-	if e.trace == nil {
-		return ""
-	}
-	return e.trace.Error()
-}
-
-func (e *appErr) Error() string {
-	return e.msg
-}
-
-func (e *appErr) Status() string {
-	return e.msg
-}
-
-func (e *appErr) AppendTrace() string {
-	return e.msg
+	msg      string
+	trace    error
+	statusCd int
 }
 
 type BadRequestErr struct {
@@ -54,48 +28,51 @@ type InternalServerErr struct {
 
 func BadRequestWrapf(format string, args ...interface{}) error {
 	err := &appErr{
-		status: http.StatusBadRequest,
+		statusCd: http.StatusBadRequest,
 	}
 
 	if len(args) == 0 {
 		err.msg = format
-		err.trace = errors.Errorf("%+v", errors.New(format))
+		//提示いただいた例だと、errors.New()をさらにErrorf（）してましたが、
+		//Errorf（)を挟む理由がよくわからなかったので、直でやりました。
+		err.trace = errors.New(format)
 		return &BadRequestErr{err}
 	}
 
 	err.msg = fmt.Sprintf(format, args...)
-	err.trace = errors.Errorf("%+v", errors.Errorf(format, args...))
+	//ここも、Errorf（)にErrorf（)を挟む理由がよくわからなかったので、直でやりました。
+	err.trace = errors.Errorf(format, args...)
 	return &BadRequestErr{err}
 }
 
 func NotFoundWrapf(format string, args ...interface{}) error {
 	err := &appErr{
-		status: http.StatusNotFound,
+		statusCd: http.StatusNotFound,
 	}
 
 	if len(args) == 0 {
 		err.msg = format
-		err.trace = errors.Errorf("%+v", errors.New(format))
+		err.trace = errors.New(format)
 		return &NotFoundErr{err}
 	}
 
 	err.msg = fmt.Sprintf(format, args...)
-	err.trace = errors.Errorf("%+v", errors.Errorf(format, args...))
+	err.trace = errors.Errorf(format, args...)
 	return &NotFoundErr{err}
 }
 
 func InternalServerWrapf(format string, args ...interface{}) error {
 	err := &appErr{
-		status: http.StatusInternalServerError,
+		statusCd: http.StatusInternalServerError,
 	}
 
 	if len(args) == 0 {
 		err.msg = format
-		err.trace = errors.Errorf("%+v", errors.New(format))
+		err.trace = errors.New(format)
 		return &InternalServerErr{err}
 	}
 
 	err.msg = fmt.Sprintf(format, args...)
-	err.trace = errors.Errorf("%+v", errors.Errorf(format, args...))
+	err.trace = errors.Errorf(format, args...)
 	return &InternalServerErr{err}
 }
