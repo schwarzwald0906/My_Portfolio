0864da496018dccfbc136c56b40a42d32945a5fd
schwarzwald0906 <schwarzwald0906@gmail.com>
2023-02-19 08:43:14 +0900

    eslintとprettierを導入

--- client/nextjs/pages/index.js
+++ client/nextjs/pages/index.js
@@ -1,14 +1,14 @@
 import Head from 'next/head'
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
-import Layout from '../components/layout';
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+import Layout from '../components/layout'
 
-import utilStyle from "../styles/util.module.css";
-import { getPostsData } from '../lib/post';
+import utilStyle from '../styles/util.module.css'
+import { getPostsData } from '../lib/post'
 
-export async function getStaticProps(){
-  const allPostsData = getPostsData();
-  console.log(allPostsData);
+export async function getStaticProps() {
+  const allPostsData = getPostsData()
+  console.log(allPostsData)
 
   return {
     props: {
@@ -17,41 +17,31 @@ export async function getStaticProps(){
   }
 }
 
-
-export default function Home({allPostsData}) {
+export default function Home({ allPostsData }) {
   return (
-  <Layout>
-    <section className={utilStyle.headingMd}>
-      <p>
-        私はフルスタックエンジニアです。
-      </p>
-    </section>
+    <Layout>
+      <section className={utilStyle.headingMd}>
+        <p>私はフルスタックエンジニアです。</p>
+      </section>
 
-    <section className={`${utilStyle.headingMd} ${utilStyle.padding1px}`}>
-      <h2>📝エンジニアのブログ</h2>
-      <div className={styles.grid}>
-        {allPostsData.map(({id,title, date ,thumbnail})=>(
-          <article key={id}>
-            <Link href={`/posts/${id}`}>
-              <img 
-                src={`${thumbnail}`}
-                className={styles.thumbnailImage}
-              />
-            </Link>
-            <br/>
-            <Link legacyBehavior href={`/posts/${id}`}>
-              <a className={utilStyle.boldText}>
-              {title}
-              </a>
-            </Link>
-          <br/>
-          <small className={utilStyle.lightText}>
-            {date}
-          </small>
-        </article>
-        ))}
-      </div>
-    </section>
-  </Layout>
-  );
+      <section className={`${utilStyle.headingMd} ${utilStyle.padding1px}`}>
+        <h2>📝エンジニアのブログ</h2>
+        <div className={styles.grid}>
+          {allPostsData.map(({ id, title, date, thumbnail }) => (
+            <article key={id}>
+              <Link href={`/posts/${id}`}>
+                <img src={`${thumbnail}`} className={styles.thumbnailImage} />
+              </Link>
+              <br />
+              <Link legacyBehavior href={`/posts/${id}`}>
+                <a className={utilStyle.boldText}>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyle.lightText}>{date}</small>
+            </article>
+          ))}
+        </div>
+      </section>
+    </Layout>
+  )
 }
